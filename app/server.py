84006cd95d8633861d131656fd76903d7a218c59
ffavas <51335777+ffavas@users.noreply.github.com>
2019-06-03 13:56:40 -0500

    Update to use the aircraft type model instead

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=19FGEI0e2ra4NDZapNuA7CeoNuH1k-z7J'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['single_engine_prop_plane','multi_engine_prop_plane','jet_engine_plane','helicopter']
 path = Path(__file__).parent
 
 app = Starlette()
